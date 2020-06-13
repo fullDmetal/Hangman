db5b6bdba03a3b6651cf93e886907fc829bd50bb
Devwrat <sdevwrat@gmail.com>
2020-06-13 10:37:38 +0530

    Updates

---
+++ precache-manifest.080b3d365c91f200f9061b0e6b54965d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5e753b4cd9b416bb99db0eb9f53871",
+    "url": "/Hangman/index.html"
+  },
+  {
+    "revision": "72b5218b335553aa2d7a",
+    "url": "/Hangman/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d63660d692b9b8a2ef8b",
+    "url": "/Hangman/static/css/main.70c6bb24.chunk.css"
+  },
+  {
+    "revision": "72b5218b335553aa2d7a",
+    "url": "/Hangman/static/js/2.895b446f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Hangman/static/js/2.895b446f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d63660d692b9b8a2ef8b",
+    "url": "/Hangman/static/js/main.562bd4e7.chunk.js"
+  },
+  {
+    "revision": "dd3cecc01685a28b25ee",
+    "url": "/Hangman/static/js/runtime-main.7662c3ac.js"
+  }
+]);
